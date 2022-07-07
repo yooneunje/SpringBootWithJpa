b13c3996c06d66d39d5e868d2a5ce396603e46bd
yooneunje <87642401+yooneunje@users.noreply.github.com>
2022-07-07 16:49:27 +0900

    project setting

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,22 +1,24 @@
 package jpabook.jpashop;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class MemberRepositoryTest {
+public class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
     @Test
+    @Transactional
+    @Rollback(value = false)
     public void testMember() throws Exception {
         //given
         Member member = new Member();
@@ -27,9 +29,10 @@ class MemberRepositoryTest {
         Member findMember = memberRepository.find(saveId);
 
         //then
-
-
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
 
     }
+    
 
 }
