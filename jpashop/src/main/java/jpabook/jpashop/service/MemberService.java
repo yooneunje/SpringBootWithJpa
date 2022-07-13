fbcb83f56b3440c7434cff702042c0a69b71d109
yooneunje <87642401+yooneunje@users.noreply.github.com>
2022-07-13 15:45:54 +0900

    develop Member domain

---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) //조회시 성능을 조금 더 최적화 시킨다.
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findByMembers = memberRepository.findByName(member.getName());
+        if (!findByMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    //회원 한명 조회
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
