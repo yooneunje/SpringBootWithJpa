fc58b719ab91310ae47137bdbe7f06a126f11a8f
yooneunje <87642401+yooneunje@users.noreply.github.com>
2022-07-26 15:48:14 +0900

    setting project data-jpa

---
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,24 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String userName;
+
+    protected Member() {
+    }
+
+    public Member(String userName) {
+        this.userName = userName;
+    }
+}
