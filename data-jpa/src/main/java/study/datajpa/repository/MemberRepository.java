9453007fd71fb07625c59cb68feb51959d843e0a
yooneunje <87642401+yooneunje@users.noreply.github.com>
2022-07-27 17:42:18 +0900

    Query Method

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,8 +1,16 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    List<Member> findByuserNameAndAgeGreaterThan(String userName, int age);
+
+    @Query("select m from Member m where m.userName = :userName and m.age = :age")
+    List<Member> findUser(@Param("userName") String userName, @Param("age") int age);
 }
