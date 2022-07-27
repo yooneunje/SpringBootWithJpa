8fc7ebdc0a1a10c0f3df636c7a7d4a1b5ea8527f
yooneunje <87642401+yooneunje@users.noreply.github.com>
2022-07-27 17:58:23 +0900

    @Query select Dto

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,9 +1,6 @@
 package study.datajpa.entity;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -11,6 +8,7 @@ import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
 public class Member {
